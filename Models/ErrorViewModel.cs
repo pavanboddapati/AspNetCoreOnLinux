1601f6baa0cdb32ae32cd2dbd4cd5f1427f34d4a
pavanb <pavan.boddapati@live.com>
2019-12-16 21:51:08 -0500

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WellsFargo.ECS.NetCoreOnLinux.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
